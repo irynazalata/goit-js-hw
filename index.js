a429a5c85f2d62035cb16eba5ce373a2769928f9
Iryna Zalata <iryna.zalata@gmail.com>
2020-09-30 00:24:26 +0300

    Add Homework 3

--- index.js
+++ index.js
@@ -439,7 +439,7 @@
 //       longestWord = el
 //     }
 //   }
-//   return typeof longestWord
+//   return longestWord
 // }
 
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
@@ -520,11 +520,11 @@
 
 // function mapArray(array) {
 //   "use strict"
-//   const numbers = new Array(array.length)
+//   const numbers = new Array(array.length);
 //   for (let i = 0; i < array.length; i += 1) {
 //     numbers[i] = array[i] * 10
 //   }
-//   return numbers
+//   return numbers;
 // }
 
 //console.log(mapArray([-2, 0, 2]))
@@ -654,5 +654,267 @@
 // 'Ошибка! Логин должен быть от 4 до 16 символов'
 // ====================================================================================================================
 
+// =============================== PRACTICE 3 ============================
+// const fn = function (n) {
+//   return n ** 2
+// }
+// const res = fn(3)
+// console.log(res)
+// -----------------------------------------------------------------
+// const fn = function (a = 0, b = 0) {
+//   return a ** b
+// }
+// const res = fn(2)
+// console.log(res)
+// -----------------------------------------------------------------
+// const createPerson = function (name, profession, age, status, experience) {
+//   return {
+//     name: name,
+//     profession: profession,
+//     age: age,
+//     status: status,
+//     experience: experience,
+//   }
+// }
+
+// let ira = createPerson("Ira", "student", 29, "married", 8)
+// console.log(ira)
+// -----------------------------------------------------------------
+// const getArguments = function () {
+//   for (let el in arguments) {
+//     console.log(el)
+//   }
+//   console.log(arguments)
+// }
+// getArguments(5, 6, 7, 8, 10)
+// -----------------------------------------------------------------
+// const fn = function (n, ...array) {
+//   for (let el of array) {
+//     array[i] = el * n
+//   }
+//   console.log(array)
+// }
+// fn(2, 3, 4, 5, 6, 7)
+// -----------------------------------------------------------------
+// let result;
+// let a = 1;
+// let b = 5;
+// result = a + b < 4 ? "Мало" : "Много";
+// console.log(result);
+
+// ===============================================================================================================
+
 // =============================== HOMEWORK 3 ============================
+
+// TASK 1 =======================================================================================
+
+// CRUD для свойств объекта
+// С - create (создать)
+// R - read (читать)
+// U - update (обновить)
+// D - delete (удалить)
+// Напиши скрипт, который, для объекта user, последовательно:
+// добавляет поле 'mood' со значением 'happy'
+// добавляет поле 'full time' со значением true
+// заменяет значение 'hobby' на 'skydiving'
+// заменяет значение 'premium' на false
+// в переменную message записывает содержимое объекта user:
+// для переменной keys присваивает массив свойств объекта, используя метод Object.keys()
+// с помощью оператора for...of
+// в формате ключ:значение
+// c переносом строки('\n')
+
+// let message = ""
+// const user = {
+//   age: 20,
+//   hobby: "html",
+//   name: "Mango",
+//   premium: true,
+// }
+
+// user.mood = "happy"
+// user["full time"] = true
+// user.hobby = "skydiving"
+// user.premium = false
+
+// const keys = Object.keys(user)
+// for (const key of keys) {
+//   message += `${key} : ${user[key]}\n`
+// }
+
+// console.log(message)
+
+// TASK 2 =======================================================================================
+// Подсчет количества свойств в объекте
+// Напиши функцию countProps(obj), которая возвращает число - количество свойств в объекте.
+// Циклы не должны использоваться
+
+// const countProps = function (obj) {
+//   "use strict"
+//   const res = Object.entries(obj).length
+//   return res
+// }
+// --------------------------------- other variant-------------------------------
+//   const countProps = function (obj) {
+//   "use strict"
+//   const res = Object.keys(obj).length
+//   return res
+// }
+// ---------------------------------------------------------------------------------
+// console.log(countProps({})) // 0
+// console.log(countProps({ a: 1, b: 1 })) // 2
+// console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })) // 5
+
+// TASK 3 =======================================================================================
+// Поиск наибольшего значения среди свойств объекта
+// Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает имя самого продуктивного(который выполнил больше всех задач).Сотрудники и кол - во выполненных задач содержатся как свойства объекта в формате "имя": "кол-во задач".
+
+// const findBestEmployee = function (employees) {
+//   "use strict"
+//   const keys = Object.keys(employees)
+//   let bestResult = 0
+//   let bestEmployee = ""
+//   for (let key of keys) {
+//     if (employees[key] > bestResult) {
+//       bestResult = employees[key]
+//       bestEmployee = key
+//     }
+//   }
+//   return bestEmployee
+// }
+// Объекты и ожидаемый результат
+// const developers = {
+//   ann: 29,
+//   david: 35,
+//   helen: 1,
+//   lorence: 99,
+// }
+// console.log(findBestEmployee(developers))
+// 'lorence'
+
+// const supports = {
+//   poly: 12,
+//   mango: 17,
+//   ajax: 4,
+// }
+// console.log(findBestEmployee(supports))
+// 'mango'
+
+// const sellers = {
+//   lux: 147,
+//   david: 21,
+//   kiwi: 19,
+//   chelsy: 38,
+// }
+// console.log(findBestEmployee(sellers))
+// 'lux'
+
+// TASK 4 =======================================================================================
+// Суммирование значений свойств объекта
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.Функция считает общую сумму зарплаты работников и возвращает ее.Каждое поле объекта, передаваемого в функцию, имеет вид "имя": "зарплата".
+
+// const countTotalSalary = function (employees) {
+//   "use strict"
+//   const salaries = Object.values(employees)
+//   let total = 0
+//   for (let salary of salaries) {
+//     total += salary
+//   }
+//   return total
+// }
+
+// Объекты и ожидаемый результат
+// const developers = {
+//   mango: 300,
+//   poly: 250,
+//   alfred: 450,
+// }
+// console.log(countTotalSalary(developers))
+// 1000
+// const supports = {
+//   kiwi: 200,
+//   lux: 150,
+//   chelsy: 150,
+// }
+// console.log(countTotalSalary(supports))
+// 500
+
+// TASK 5 =======================================================================================
+// Оператор in и метод push
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
+// Функция возвращает массив значений определенного свойства prop из каждого объекта в массиве.
+// Используй метод push для добавления значения в массив и оператор in для проверки наличия свойства в объекте.
+
+// function getAllPropValues(array, prop) {
+//   "use strict"
+//   const result = []
+//   for (const el of array) {
+//     for (const key in el) {
+//       if (key === prop) {
+//         result.push(el[key])
+//         break
+//       }
+//     }
+//   }
+//   return result
+// }
+
+// Объекты и ожидаемый результат
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Радар", price: 1280, quantity: 2 },
+//   { name: "Радар", price: 1320, quantity: 1 },
+//   { name: "Сканер", price: 2700, quantity: 1 },
+//   { name: "Сканер", price: 2500, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 2 },
+// ]
+
+// console.log(getAllPropValues(products, "name"))
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+// console.log(getAllPropValues(products, "quantity"))
+// [4, 2, 1, 1, 3, 7, 2]
+
+// console.log(getAllPropValues(products, "category"))
+//  []
+
+// TASK 6 =======================================================================================
+// Суммирование значений свойства из массива объектов
+// Напиши функцию calculateTotalPrice(allProdcuts, productName), которая получает массив объектов и имя продукта (значение свойства name). Возвращает общую стоимость продукта (цена умноженная на количество).
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+// function calculateTotalPrice(array, prop) {
+//   "use strict"
+//   let sum = 0
+//   let total = 0
+//   for (const el of array) {
+//     const values = Object.values(el)
+//     const [name, ...args] = values
+//     total = args[0] * args[1]
+//     if (prop === name) {
+//       sum += total
+//     }
+//   }
+//   return sum
+// }
+
+// Объекты и ожидаемый результат
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Радар", price: 1280, quantity: 2 },
+//   { name: "Радар", price: 1320, quantity: 1 },
+//   { name: "Сканер", price: 2700, quantity: 1 },
+//   { name: "Сканер", price: 2500, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 2 },
+// ]
+// console.log(calculateTotalPrice(products, "Радар"))
+// 9080
+// console.log(calculateTotalPrice(products, 'Сканер'));
+// 10200
+// console.log(calculateTotalPrice(products, 'Захват'));
+// 2400
+// console.log(calculateTotalPrice(products, 'Дроид'));
+// 2800
 // ===============================================================================================================
