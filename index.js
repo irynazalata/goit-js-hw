8db50b3998c9b955de2ff9152ea3b0714b56a161
Iryna Zalata <iryna.zalata@gmail.com>
2020-10-01 13:06:42 +0300

    Add prettier

--- index.js
+++ index.js
@@ -899,16 +899,33 @@
 //   return sum
 // }
 
+// --------------------------- 2variant-----------------------
+//   function calculateTotalPrice(array, prop) {
+//   'use strict';
+//   let sum = 0;
+//   let total = 0;
+//   for (const el of array) {
+//     if (prop === el.name) {
+//       total = el.price * el.quantity;
+//       sum += total;
+//     }
+//   }
+//   return sum;
+//   }
+// -----------------------------------------------------
+
 // Объекты и ожидаемый результат
 // const products = [
-//   { name: "Радар", price: 1300, quantity: 4 },
-//   { name: "Радар", price: 1280, quantity: 2 },
-//   { name: "Радар", price: 1320, quantity: 1 },
-//   { name: "Сканер", price: 2700, quantity: 1 },
-//   { name: "Сканер", price: 2500, quantity: 3 },
-//   { name: "Дроид", price: 400, quantity: 7 },
-//   { name: "Захват", price: 1200, quantity: 2 },
-// ]
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Радар', price: 1280, quantity: 2 },
+//   { name: 'Радар', price: 1320, quantity: 1 },
+//   { name: 'Сканер', price: 2700, quantity: 1 },
+//   { name: 'Сканер', price: 2500, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+
+// console.table(products);
 // console.log(calculateTotalPrice(products, "Радар"))
 // 9080
 // console.log(calculateTotalPrice(products, 'Сканер'));
@@ -918,3 +935,12 @@
 // console.log(calculateTotalPrice(products, 'Дроид'));
 // 2800
 // ===============================================================================================================
+// const isApet = function (array) {
+//   const phrase = ' is a pet';
+//   for (let i = 0; i < array.length; i++) {
+//     array[i] += phrase;
+//   }
+//   return array;
+// };
+
+// console.log(isApet(['Mango', 'Poly', 'Ajax']));
