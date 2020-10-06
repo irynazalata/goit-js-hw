7a2d528b82fd77382fe8b6ffed271c83b22a6f46
Iryna Zalata <iryna.zalata@gmail.com>
2020-10-06 22:39:16 +0300

    Add Homework 5

--- js/index5.js
+++ js/index5.js
@@ -5,6 +5,11 @@
 // console.log(dog.barks); // true
 // console.log(dog.eats);
 // console.log(dog);
+
+// class Student {
+//   constructor() {}
+// }
+// console.log(typeof Student);
 // ----------------------------------------------------------------------------------
 // ================================================ HOMEWORK ==========================================
 // Task 1 -----------------------------------------------------------------------------------
@@ -30,3 +35,285 @@
 // const poly = new Account('Poly', 'poly@mail.com');
 // console.log(poly.getInfo());
 // Task 2 -----------------------------------------------------------------------------------
+// class
+// Напиши класс User для создания пользователя со следующим свойствами:
+// name - строка
+// age - число
+// followers - число
+// Добавь метод getInfo(), который, выводит строку: User ${ имя } is ${ возраст } years old and has ${ кол - во фоловеров } followers
+// class User {
+//   constructor(name, age, followers) {
+//   this.name = name;
+//   this.age = age;
+//   this.followers = followers;
+//   }
+
+//   getInfo() {
+//     return (`User ${this.name} is ${this.age} years old and has ${this.followers} followers`)
+//   };
+// };
+
+// console.log(typeof User);
+// 'function'
+// const mango = new User('Mango', 2, 20);
+// console.log(mango.getInfo());
+// 'User Mango is 2 years old and has 20 followers'
+
+// console.log(typeof mango.getInfo);
+// 'function'
+// const poly = new User( 'Poly', 3, 17);
+// console.log(poly.getInfo());
+// 'User Poly is 3 years old and has 17 followers'
+// Task 3 -----------------------------------------------------------------------------------
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров. При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+// Добавь методы класса:
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получает товар и, если он есть, удаляет его из текущих
+// class Storage {
+//   constructor(array) {
+//     this.items = array;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(item) {
+//     this.items.push(item);
+//   }
+//   removeItem(item) {
+//     for (let i = 0; i < this.items.length; i++) {
+//       if (this.items[i] === item) {
+//         this.items.splice(i, 1);
+//         return this.items;
+//       }
+//     }
+//   }
+// }
+
+// console.log(typeof Storage);
+// 'function'
+// const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
+// const storage = new Storage(goods);
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+// storage.addItem('Дроид');
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+// Task 4 -----------------------------------------------------------------------------------
+// переиспользование методов класса
+// Напиши класс StringBuilder. На вход он получает один параметр - строку, которую записывает в свойство _value.
+// Добавь классу следующий функционал:
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает параметр str (строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает параметр str (строку) и добавляет ее в начало value
+// Метод pad(str) - получает параметр str (строку) и добавляет ее в начало и в конец _value
+// Метод pad должен использовать методы append и prepend
+
+// class StringBuilder {
+//   constructor(str) {
+//     this._value = str;
+//   }
+//   get value() {
+//     return this._value;
+//   }
+//   append(str) {
+//     this._value += str;
+//   }
+//   prepend(str) {
+//     this._value = str + this._value;
+//   }
+//   pad(str) {
+//     this.append(str);
+//     this.prepend(str);
+//   }
+// }
+
+// console.log(typeof StringBuilder);
+// 'function'
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
+// Task 5 -----------------------------------------------------------------------------------
+// класс
+// Напиши класс Car с указанными свойствами и методами.
+
+// class Car {
+/*
+ * Добавь `статический` метод
+ * `getSpecs(car)`, который принимает
+ * объект-машину как параметр
+ * и возвращает шаблонную строку
+ * со свойствами и значениями объекта.
+ * Свойства:
+ *   maxSpeed,
+ *   speed,
+ *   isOn,
+ *   distance,
+ *   price
+ * Пример строки, полученной этим методом:
+ * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+ */
+
+/*
+ * Конструктор получает объект настроек.
+ *
+ * Добавь свойства будущеего экземпляра класса:
+ *  speed - текущая скорость,
+ *          начальное значение `0`
+ *  price - цена автомобиля
+ *  maxSpeed - максимальная скорость
+ *  isOn - заведен ли автомобиль.
+ *         Значения `true` или `false`,
+ *         начальное значение false
+ *  distance - пробег в километрах,
+ *             начальное значение `0`
+ */
+// constructor() {}
+
+/*
+ * Добавь геттер и сеттер
+ * для свойства `price`, который будет
+ * работать с свойством цены автомобиля.
+ *
+ * ВАЖНО: для записи методов get и set
+ * значение параметра записывают с
+ * подчеркиванием. См. ниже пример 1.
+ */
+
+/*
+ * Метод, который заводит автомобиль
+ * Записывает в свойство `isOn` значение `true`
+ */
+// turnOn() {}
+
+/*
+ * Метод, чтобы заглушить автомобиль
+ * Этот метод должен записывать
+ * в свойство isOn значение false,
+ * и сбрасывать текущую скорость до 0
+ */
+// turnOff() {}
+
+/*
+ * Этот метод должен добавлять
+ * к свойству `speed` полученное
+ * значение, при условии,
+ * что результирующая скорость
+ * не больше чем значение свойства `maxSpeed`
+ */
+// accelerate(value) {}
+
+/*
+ * Этот метод должен отнимать
+ * от свойства `speed`
+ * полученное значение, при условии,
+ * что результирующая скорость не меньше 0
+ */
+
+// decelerate(value) {}
+
+/*
+ * Этот метод должен добавлять к свойству
+ * `distance` пробег в километрах,
+ * т.е. hours * speed,
+ * но только в том случае,
+ * если машина заведена!
+ */
+//   drive(hours) {}
+// }
+
+// class Car {
+//   static getSpecs(car) {
+//     return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
+//   }
+//   constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
+//     this.speed = speed;
+//     this._price = price;
+//     this.maxSpeed = maxSpeed;
+//     this.isOn = isOn;
+//     this.distance = distance;
+//   }
+//   get price() {
+//     return this._price;
+//   }
+//   set price(value) {
+//     return (this._price = value);
+//   }
+//   turnOn() {
+//     this.isOn = true;
+//   }
+//   turnOff() {
+//     this.isOn = false;
+//     this.speed = 0;
+//   }
+//   accelerate(value) {
+//     if (value <= this.maxSpeed) {
+//       this.speed += value;
+//     } else if (value + this.speed > this.maxSpeed) {
+//       this.speed = this.maxSpeed;
+//     }
+//   }
+//   decelerate(value) {
+//     if (value <= this.speed) {
+//       this.speed -= value;
+//     } else if (value - this.speed > 0) {
+//       this.speed = 0;
+//     }
+//   }
+//   drive(hours) {
+//     if (this.isOn) {
+//       this.distance += hours * this.speed;
+//     }
+//   }
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+
+// mustang.decelerate(20);
+// console.log(Car.getSpecs(mustang));
+
+// mustang.drive(1);
+// mustang.turnOff();
+
+// console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+
+//console.log(mustang.price); // 2000
+//mustang.price = 4000;
+//console.log(mustang.price); // 4000
+// ---------------------------------------------------------------------------------------------
